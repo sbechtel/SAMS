95c05e7b9dae942d00b548d6a56781071d439f57
Felix Breidenstein <felixbreidenstein@gmx.de>
2012-10-05 04:23:56 +0200

    Only get messages, save messages to files for every contact, pep8 correction

--- sams_receive.py
+++ sams_receive.py
@@ -1,13 +1,21 @@
 #! /usr/bin/env python
+import pickle
 
 from libsams import Addressbook, Receiver
 from os import environ
 from rsa import PublicKey, PrivateKey
 from datetime import datetime
 
-dt = datetime(2012,10,05,00,00,00)
+now = datetime.now()
 home = environ.get('HOME')
 user = environ.get('USER')
+last_receive = '{home}/.sams/messages/last'.format(home=home)
+try:
+    with open(last_receive, 'rw') as last:
+        dt = pickle.load(last)
+except:
+    dt = datetime(1990, 1, 1, 00, 00)
+
 addressbookfile = '{home}/.sams/addressbook.csv'.format(home=home)
 addressbook = Addressbook(addressbookfile)
 my_address = addressbook.get_by_name(user)
@@ -22,4 +30,26 @@ privkey = PrivateKey.load_pkcs1(keydata)
 
 receiver = Receiver(privkey, pubkey)
 messages = receiver(dt)
-print messages
+
+print 'You have {new} new Messages!'.format(new=len(messages))
+
+for message in messages:
+    sender = message['author']
+    msg = message['msg']
+    date = message['date']
+    date = ('{day:02d}.{month:02d}.{year}'
+            '{hour:02d}:{minute:02d}').format(day=date.day, month=date.month,
+                                              year=date.year,
+                                              hour=date.hour,
+                                              minute=date.minute)
+
+    uri = '{home}/.sams/messages/{sender}.txt'.format(home=home,
+                                                      sender=sender)
+    with open(uri, 'a') as file:
+        file.write(date + '\n')
+        file.write(msg)
+        file.write('#' * 10 + '\n')
+
+last_receive = '{home}/.sams/messages/last'.format(home=home)
+with open(last_receive, 'w') as last:
+    pickle.dump(now, last)
